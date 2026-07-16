1abb3a76ad05c4fdb1a5a09af66e1551fb980ebc
tyronhuggins32-cmd <tyronhuggins32@gmail.com>
2026-07-16 10:31:02 -0400

    Update game.js

--- game.js
+++ game.js
@@ -6576,10 +6576,7 @@ if (goingUp) {
       ).length;
 
       const populationCap = Math.min(
-  320,
-  190 + this.day * 12
-);
-  640,
+640,
   380 + this.day * 24
 );
       if (living >= populationCap) return;
